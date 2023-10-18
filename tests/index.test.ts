1fbac2f8fdfa7f7434e0e9b425558b2dd68fe838
pacifiquem <Murangwa Pacifique>
2023-10-18 11:04:07 +0200

    [test]: test for fetch-facts function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,1 +1,14 @@
-console.log(`init`);
+import fetchFacts from "..";
+import { expect, it, test } from "vitest";
+
+test("Fetch Data Function", () => {
+    it("Should return 1 fact by default", async () => {
+        const facts = await fetchFacts();
+        expect(facts).toHaveLength(1);
+    });
+
+    it("Should return number of specified facts", async () => {
+        const facts = await fetchFacts({limit: 10});
+        expect(facts).toHaveLength(10);
+    });
+});
